58630b60bd1b5d28198bac33c862a7670f6b4c24
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-04-20 18:03:16 +0100

    Fix pylint errors

--- models/logger.py
+++ models/logger.py
@@ -1,12 +1,15 @@
+"""
+logger.py - Provides function to maintain an app logger to record warnings and errors.
+"""
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(app=None):
+def setup_logger():
+    """Set up the logger for the application."""
     logger = logging.getLogger("app_logger")
     logger.setLevel(logging.INFO)
-    
-    """Set up the logger for the application."""
+
     # Ensure the logs directory exists
     if not os.path.exists('logs'):
         os.makedirs('logs')
@@ -16,13 +19,13 @@ def setup_logger(app=None):
     if not logger.handlers:
         # Set up the RotatingFileHandler
         handler = RotatingFileHandler(log_file_path, maxBytes=1_000_000, backupCount=5)
-        
+
 
         # Create a formatter and set it for the handler
         formatter = logging.Formatter(
                 "[%(asctime)s] %(levelname)s in %(funcName)s: %(message)s"
             )
-        
+
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
--- models/mailer.py
+++ models/mailer.py
@@ -1,57 +1,67 @@
-# mailer.py
+"""
+mailer.py - Provide functions to send emails from the app.
+"""
+import logging
 import smtplib
+
 from email.message import EmailMessage
 from jinja2 import Environment, FileSystemLoader
-import os
-import logging
+from flask import flash
+
 import config
+from utils import get_pretty_datetime_str
 
 logger = logging.getLogger("app_logger")
 
 # Setup Jinja2 environment to load from templates folder
 env = Environment(loader=FileSystemLoader(config.EMAIL_TEMP_DIR))
 
-   
 
-        
-        
-        
 def send_email_notification(booking_id, booking):
-    
+    """Send email based on booking record status.
+
+    Args:
+        booking_id (str): Booking ID
+        booking (dict): The booking record.
+
+    Returns:
+        Boolean: True on success, otherwise False.
+    """
+
     recipient = booking.get("original_sheet_data", {}).get("Email address")
-    
+
     if not recipient:
-        logger.warning(f"No email address found for booking: {booking_id} {booking}")
+        logger.warning("No email address found for booking: %s: %s", booking_id, booking)
         return False
-    
+
     context = {
         "leader": booking.get("Leader", "Leader"),
         "arriving": booking.get("Arriving", "a future date"),
         "campsite": booking.get("Campsite", "the campsite"),
         "booking_id": booking_id
     }
-    
+
     if booking["Status"] == "Confirmed":
         text_template = env.get_template("booking_confirmed.txt")
         html_template = env.get_template("booking_confirmed.html")
-    
+
     elif booking["Status"] == "Cancelled":
         text_template = env.get_template("booking_cancelled.txt")
         html_template = env.get_template("booking_cancelled.html")
-    
+
     elif booking["Status"] == "Pending":
         text_template = env.get_template("booking_pending.txt")
         html_template = env.get_template("booking_pending.html")
-    
+
     else:
-        logger.warning(f"No email to be sent for status: {booking.get("Status")} {booking}")
+        logger.warning("No email to be sent for status: %s: %s", booking.get("Status"), booking)
         return False
-    
+
     try:
         body_text = text_template.render(context)
         body_html = html_template.render(context)
-    except Exception as e:
-        logger.error(f"Error rendering email templates: {e} {booking}")
+    except smtplib.SMTPException as e:
+        logger.error("Error rendering email templates: %s: %s", e, booking)
         return False
 
     msg = EmailMessage()
@@ -64,13 +74,14 @@ def send_email_notification(booking_id, booking):
     msg.add_alternative(body_html, subtype="html")
 
     try:
-        #with smtplib.SMTP("smtp.office365.com", 587) as server:
-        #    server.starttls()
-        #    server.login(config.EMAIL_USER, config.EMAIL_PASS)
-        #    server.send_message(msg)
-        #booking["email_confirmation_sent"] = get_pretty_datetime_str(include_seconds=True)
+        with smtplib.SMTP("smtp.office365.com", 587) as server:
+            server.starttls()
+            server.login(config.EMAIL_USER, config.EMAIL_PASS)
+            server.send_message(msg)
+        booking["email_confirmation_sent"] = get_pretty_datetime_str(include_seconds=True)
         return True
-    except Exception as e:
-        logger.error(f"Failed to send email to {recipient}: {e}")
-        flash(f"Failed to send email to {recipient}: {e}", "danger")
+    except smtplib.SMTPException as e:
+        logger.error("Failed to send email to %s: %s", recipient, e)
+        msg = f"Failed to send email to {recipient}: {e}"
+        flash(msg, "danger")
         return False
