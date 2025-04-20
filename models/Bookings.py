ee943f0cdc6a48370ac82485b1988c62060ff585
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-04-20 09:46:29 +0100

    Update Bookings.py
    Remove whitespace

--- models/Bookings.py
+++ models/Bookings.py
@@ -34,7 +34,7 @@ class Bookings:
         self.json_path = Path(CACHE_DIR, "bookings.json")
         self.data = {}
         self._load()
-    
+
     def GetStates(self):
         return {"names": status_options, "transitions": status_transitions}
 
@@ -49,12 +49,12 @@ class Bookings:
             return secs_to_hr(int(time.time()) - self.data["timestamp"])
         else:
             return "NEVER!"
-    
+
     def Get(self, booking_id=None):
-        
+
         # ToDo - Only added during dev. Force loading of JSON each time to allow me to edit data
         self._load()
-            
+
         # If booking_id is provided, return only that booking
         if booking_id:
             # Fetch the specific booking data by booking_id
@@ -85,9 +85,9 @@ class Bookings:
         return to_status in status_transitions.get(from_status, [])
 
     def ChangeStatus(self, booking_id, new_status, description=None):
-        
+
         booking = self.data.get("bookings", {}).get(booking_id)
-        
+
         if new_status in ("Cancelled", "Pending"):
             if not description:
                 msg = (
@@ -104,17 +104,17 @@ class Bookings:
         elif description:
             self.logger.warning(f"Unexpected description for state {new_status} (booking {booking_id}): {description}")
             return False
-        
+
         old_status = booking.get("Status")
-        
+
         if self._apply_status_change(booking_id, new_status):
             send_email_notification(booking_id, booking)
             #handle_calendar_entry(booking_id, booking)
             self._add_to_notes(booking, f"Status changed [{old_status}] > [{new_status}]")
-            
+
             self._save()
-        
-        
+
+
     def ModifyFields(self, booking_id, updates: dict) -> bool:
 
         editable_fields = {"Number", "Arriving", "Departing"}
@@ -125,17 +125,17 @@ class Bookings:
         if not booking:
             flash(f"Cannot update booking {booking_id} as not found in database.", "danger")
             return False
-        
+
         for field, new_value in updates.items():
             if field not in editable_fields:
                 self.logger.warning(f"Bookings/Update tried to edit field {field} which is not in my list: {booking_id}")
                 continue
-            
+
             old_value = booking.get(field)
-            
+
             if old_value == new_value:
                 continue  # No change, skip
-        
+
             booking[field] = new_value
 
             if field in ("Arriving", "Departing"):
@@ -146,18 +146,18 @@ class Bookings:
                 new_value_str = new_value
 
             self._add_to_notes(booking, f"{field} changed from [{old_value_str}] to [{new_value_str}]")
-    
+
         self._save()
 
 
     def _apply_status_change(self, booking_id, to_status):
-        
+
         booking = self.data.get("bookings", {}).get(booking_id)
-        
+
         if not booking:
             flash(f"Cannot update booking {booking_id} as not found in database.", "danger")
             return False
-        
+
         from_status = booking.get("Status")
 
         if not self._can_transition(from_status, to_status):
@@ -176,14 +176,14 @@ class Bookings:
                 return False
 
         booking["Status"] = to_status
-                
+
         self._save()
         return True
 
 
-        
+
     def _add_to_notes(self, booking, new_note):
-        
+
         timestamp = get_pretty_datetime_str(include_seconds=True)
         new_note_entry = f"[{timestamp}]: {new_note}"
 
@@ -191,8 +191,8 @@ class Bookings:
         booking["Notes"] = new_note_entry + ("\n" + old_value if old_value else "")
         self._save()
 
-    
-    
+
+
     def _save(self):
         # Searilise the BookingType ENUM to string before saving
         for booking in self.data.get("bookings", {}).values():
@@ -202,13 +202,13 @@ class Bookings:
         with open(self.json_path, 'w') as f:
             #self.logger.info(f"Saving bookings data to file")
             json.dump(self.data, f, indent=2)
-            
-            
+
+
     def _load(self):
         if self.json_path.exists():
             with open(self.json_path, 'r') as f:
                 self.data = json.load(f)
-                
+
                 # Deseralise BookingType string back to ENUM
                 for booking in self.data.get("bookings", {}).values():
                     bt_raw = booking.get("booking_type")
@@ -216,15 +216,15 @@ class Bookings:
                         booking["booking_type"] = BookingType[bt_raw]
                     except (KeyError, TypeError):
                         self.logger.warning(f"Unknown or missing booking_type: {bt_raw}")
-                
+
                 self._auto_update_statuses()
-    
+
         else:
             self.data = {
                 "timestamp": int(time.time()),
                 "bookings": {}
             }
-    
+
     def _auto_update_statuses(self):
         now = int(time.time())
 
@@ -261,33 +261,33 @@ class Bookings:
     #z
     ## Function to load a sheet of data in dict format into our booking structure
     def AddNewData(self, sheet_bookings, booking_type):
-        
+
         bookings_added = 0
-        
+
         if "timestamp" in sheet_bookings and sheet_bookings["timestamp"]:
-            
+
             self.data["timestamp"] = sheet_bookings["timestamp"]
-            
+
             #
             ## Need to normalise the new data from Sheet to our structure
             for sb in sheet_bookings["sheet_data"]:
-                
+
                 #
                 ## Create MD5 of sheet line item so we can track if its new or seen before
                 new_booking_md5 = self._md5_of_dict(sb)
                 booking_id, booking = self._find_booking_by_md5(new_booking_md5)
-                
+
                 if not booking:
-                    
+
                     start_dt = datetime.strptime(sb["Arrival Date / Time"], "%d/%m/%Y %H:%M:%S")
-                    
+
                     # Parse the departure time and replace the time part of arrival
                     dep_time = datetime.strptime(sb["Departure Time"], "%H:%M:%S").time()
                     end_dt = start_dt.replace(hour=dep_time.hour, minute=dep_time.minute, second=0)
-                    
+
                     existing_ids = list(self.data["bookings"].keys())
                     new_booking_id = generate_next_booking_id(existing_ids, booking_type, start_dt.year)
-                    
+
                     new_booking = {
                         new_booking_id: {
                             "original_sheet_md5": new_booking_md5,
@@ -305,11 +305,11 @@ class Bookings:
                             "Notes": ""
                         }
                     }
-                    
+
                     self._add_to_notes(new_booking.get(new_booking_id), "Pulled from sheets")
                     self.data["bookings"].update(new_booking)
                     bookings_added += 1
 
             self._save()
-            
+
         return bookings_added
