6c2c8cc3e7bfec70ade302879871e55dc399a6c5
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-04-23 18:01:17 +0100

    Remove cache folder as unused

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ APP_ENV = os.getenv("APP_ENV", "development")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-CACHE_DIR = os.path.join(BASE_DIR, "cache")
 LOG_DIR = os.path.join(BASE_DIR, "logs")
 DATA_DIR = os.path.join(BASE_DIR, "data")
 EMAIL_TEMP_DIR = os.path.join(BASE_DIR, "email_templates")
@@ -23,7 +22,6 @@ LOG_FILENAME = "app.log"
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILENAME)
 
 # Make sure these directories exist when config is loaded
-os.makedirs(CACHE_DIR, exist_ok=True)
 os.makedirs(LOG_DIR, exist_ok=True)
 os.makedirs(DATA_DIR, exist_ok=True)
 
