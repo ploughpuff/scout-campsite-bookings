5c21acce3712853a021538ac3297dc29214e5f41
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-04-24 17:05:47 +0100

    Added SITENAME

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ else:
     load_dotenv(".env")
 
 APP_SECRET_KEY = os.getenv("SECRET_KEY")
+SITENAME = os.getenv("SITENAME")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
