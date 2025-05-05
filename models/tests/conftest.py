6cc93f3ebf480243c58f901b527da54a55b0627e
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-05-05 01:01:37 +0100

    Add fake field mapping data to satisfy pytest

--- models/tests/conftest.py
+++ models/tests/conftest.py
@@ -2,7 +2,9 @@
 conftest.py
 """
 
+import json
 import os
+from pathlib import Path
 
 import pytest
 
@@ -13,3 +15,55 @@ def set_test_env():
     os.environ["APP_ENV"] = "test"
     yield
     del os.environ["APP_ENV"]  # Optionally clean up after the test
+
+
+FIELD_MAPPING_PATH = Path("field_mappings.json")
+
+
+def pytest_sessionstart(_session):
+    """Write a dummy field_mappings,json file to root so pytest can use it"""
+    if not FIELD_MAPPING_PATH.exists():
+        FIELD_MAPPING_PATH.parent.mkdir(parents=True, exist_ok=True)
+        dummy_data = {
+            "booking_types": {
+                "district_day_visit": {
+                    "prefix": "ABC",
+                    "fields": {
+                        "leader": {
+                            "name": "name_of_lead_person",
+                            "email": "email_address",
+                            "phone": "mobile_number_for_lead_person",
+                        },
+                        "site": {
+                            "group_name": "somewhere_scout_group",
+                            "group_size": "number_of_people",
+                        },
+                    },
+                },
+                "school": {
+                    "prefix": "SCH",
+                    "fields": {
+                        "leader": {
+                            "name": "name_of_teacher",
+                            "email": "email_address",
+                            "phone": "mobile_number_for_teacher",
+                        },
+                        "site": {
+                            "group_name": "school_name",
+                            "group_size": "number_of_people",
+                        },
+                    },
+                },
+            },
+            "sheets": [
+                {
+                    "name": "Fake Test Data",
+                    "use": true,
+                    "id": "0123456789ABCDEF0123456789ABCDEF0123456789AB",
+                    "range": "2025!A:E",
+                    "booking_type": "district_day_visit",
+                },
+            ],
+        }
+
+        FIELD_MAPPING_PATH.write_text(json.dumps(dummy_data), encoding="utf-8")
