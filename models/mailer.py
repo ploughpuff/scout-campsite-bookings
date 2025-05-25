fcb383c84f61c25c0cb0b36406d48c6c8ab1b5ad
ploughpuff <33969763+ploughpuff@users.noreply.github.com>
2025-05-25 22:31:07 +0100

    Add sitename to email subject

--- models/mailer.py
+++ models/mailer.py
@@ -115,7 +115,7 @@ def _create_email_message(context, rec: LiveBooking, subject_append_str: str = "
 
     arriving_str = get_pretty_date_str(rec.booking.arriving)
     msg = EmailMessage()
-    subject = f"Booking for {arriving_str}: {rec.booking.id} {rec.tracking.status.upper()}"
+    subject = f"{config.SITENAME} Booking for {arriving_str}: {rec.booking.id} {rec.tracking.status.upper()}"
     if subject_append_str:
         subject += f" ({subject_append_str})"
     msg["Subject"] = subject
